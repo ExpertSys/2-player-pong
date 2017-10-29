1175ca4e9ca11d5d11ed357426ebf3424c55604b
Jordan Brown <ampzgunz@hotmail.com>
2017-10-29 00:03:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,30 @@
 <title>2 Player Pong</title>
 <link rel = "stylesheet" type = "text/css" href = "css/style.css"/>
 <link href="https://fonts.googleapis.com/css?family=Contrail+One" rel="stylesheet">
+<script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
 </head>
 
 <body>
-<h1>Red: <span id = "red-score">0</span></h1>
-<h1>Blue: <span id = "green-score">0</span></h1>
 
+
+
+<div class = "gameframe">
+  <h1>Red: <span id = "red-score">0</span></h1>
+  <h1>Blue: <span id = "green-score">0</span></h1>
+  <div class = "player1-btns">
+        <span class = "btn">1</span>
+        <span class = "btn">3</span>
+  </div>
+
+  <div class = "player2-btns">
+        <span class = "btn">Q</span>
+        <span class = "btn">E</span>
+  </div>
 <canvas id="game"></canvas>
+
+</div>
 <script src="js/index.js"></script>
 
 </body>
